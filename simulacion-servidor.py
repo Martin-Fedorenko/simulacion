b88a682c68f5ab2b3d3de21ab7afe383e870673d
Martin Fedorenko <MartinFedorenkoLK1288@gmail.com>
2024-07-24 22:53:27 -0300

    commit no tan ciego

--- simulacion-servidor.py
+++ simulacion-servidor.py
@@ -4,6 +4,12 @@ import time
 import numpy
 from datetime import datetime, timedelta
 
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+RESET = '\033[0m'
+
 ### C.I. ###
 
 # VARIABLES DE CONTROL
@@ -13,8 +19,8 @@ IPC = 0
 VC = 0
 M = 0
 
-INS_PETICION = 0 #cantidad instrucciones promedio por peticion
-MEM_PETICION = 0 #cantidad memoria promedio por peticion (KB)
+INS_PETICION = 50000 #cantidad instrucciones promedio por peticion
+MEM_PETICION = 10 #cantidad memoria promedio por peticion (KB)
 
 # VARIABLES DE RESULTADO
 CVCS = 0
@@ -27,8 +33,9 @@ P = 0
 
 # VARIABLES AUXILIARES
 T=0
-TF = 24 * 365 * 10 # 10 años de simulación 24 horas al dia
-FECHA_INICIAL = datetime(2024, 1, 1)
+#TF = 24 * 365 * 10 # 10 años de simulación 24 horas al dia
+TF = 24*4
+FECHA_INICIAL = datetime(2024, 12, 22)
 SPRF = 0 #sumatoria
 SPCF = 0 #sumatoria
 SPPR = 0 #sumatoria
@@ -47,8 +54,8 @@ while True:
         H = int(input("Cantidad de hilos (H): "))
         VC = int(input("Velocidad clock en Hz, NO en Ghz (VC): "))
         IPC = int(input("Cantidad de instrucciones por ciclo (IPC): "))
-        CAP_CPU = N*H*IPC*VC*3600/INS_PETICION
-        CAP_RAM = M/MEM_PETICION
+        CAP_CPU = (N*H*IPC*VC*3600)/(INS_PETICION)
+        CAP_RAM = (M/MEM_PETICION)
 
         break
     except ValueError:
@@ -64,11 +71,12 @@ def obtener_PH(): #Peticiones por Hora
 # 0.08% de probabilidad de recibir ataque DDoS por hora
 def ocurrencia_ataque_DDoS():
     global DDOS_FLAG
-    probabilidad = np.random.rand()
-    if (probabilidad < 0.08) :
-        DDOS_FLAG = true
-    else :
-        DDOS_FLAG = false
+    probabilidad = numpy.random.rand()
+
+    if (probabilidad < 0.08):
+        DDOS_FLAG = True
+    else:
+        DDOS_FLAG = False
 
 def obtener_hora():
     global T
@@ -82,58 +90,59 @@ def obtener_fecha():
     return fecha_actual
     
 def es_comienzo_trimestre():
-    mes = obtener_fecha.month
-    dia = obtener_fecha.dia
+    mes = obtener_fecha().month
+    dia = obtener_fecha().day
     
     return (mes == 1 and dia == 1) or (mes == 4 and dia == 1) or (mes == 7 and dia == 1) or (mes == 10 and dia == 1)
     
 def es_comienzo_mes():
-    dia = obtener_fecha.dia
+    dia = obtener_fecha().day
     return dia == 1
 
 def porcentaje_aumento_evento(mes_evento, dia_evento):
     fecha = obtener_fecha()
     evento = datetime(fecha.year, mes_evento, dia_evento)
-    
-    dias_restantes = (evento-fecha).days
-    
-    if fecha == evento
+
+    if fecha.day == dia_evento and fecha.month == mes_evento:
         return 1.15
-    else
+    else:
         return 1
     
 def porcentaje_aumento_evento_anticipado(mes_evento, dia_evento):
     fecha = obtener_fecha()
     evento = datetime(fecha.year, mes_evento, dia_evento)
-    
+
     dias_restantes = (evento-fecha).days
     
-    if dias_restantes > 0 and dias_restantes <=5
-        if fecha.day == dia_evento and fecha.month == mes_evento
-        return 1+0.15*(-dias_restantes+6)
-        else
-            return 1
+    if dias_restantes >= 0:
+        return 1+0.15*(-dias_restantes+5)
+    else:
+        return 1
     
 
 def mantenimiento():
-    global CAP_CPU, CAP_RAM
+    global CAP_CPU, CAP_RAM, MANTENIMIENTO_ACTIVO
     CAP_CPU = CAP_CPU*0.8
     CAP_RAM = CAP_RAM*0.8
     MANTENIMIENTO_ACTIVO = 2
     
 def sin_mantenimiento():
-    global CAP_CPU, CAP_RAM
+    global CAP_CPU, CAP_RAM, MANTENIMIENTO_ACTIVO
     CAP_CPU = CAP_CPU/0.8
     CAP_RAM = CAP_RAM/0.8
     MANTENIMIENTO_ACTIVO = 0
 
-
 def resultados():
-    global T
+    global T, SPPR, SPCF,SPRF
 
-    PRF = SPRF * MEM_PETICION / CVCS
-    PCF = SPCF * INS_PETICION / CVCS
-    PPR = SPPR / CVCS
+    if(CVCS!=0):
+        PRF = (SPRF * MEM_PETICION) / (CVCS)
+        PCF = (SPCF * INS_PETICION) / (CVCS * 3600)
+        PPR = SPPR / CVCS
+    else:
+        PRF = 0
+        PCF = 0
+        PPR = 0
 
     print(f"Cantidad de veces capacidad del servidor superada {CVCS}")
     print(f"Promedio de capacidad de RAM faltante (KB): {PRF}")
@@ -142,80 +151,98 @@ def resultados():
 
 
 def realizar_simulacion():
-    global T, P, CAP_CPU, CAP_RAM, CVCS
+    global T, P, CAP_CPU, CAP_RAM, CVCS, MANTENIMIENTO_ACTIVO, SPPR, SPCF,SPRF
     while True:
+        fecha = obtener_fecha()
         
         ### AVANZO EN EL TIEMPO EN UN DELTA T ###
+
+        print(f"{BLUE}Fecha: {fecha}{RESET}")
         
         T=T+1
 
         ### GENERO, CALCULO O USO TODO LO QUE ENTRA ###
 
-        ph = obtener_PH()
+        ph = obtener_PH() #OK
         
-        #PH horas pico
+        #PH horas pico #OK
         
-        if obtener_hora() in [18, 19, 20, 21, 22]:
+        if obtener_hora() in [18, 19, 20, 21, 22]: 
             ph = ph * 3
-            
+        
         #PH ante ocurrencia de ataque DDOS
 
         ocurrencia_ataque_DDoS()
 
         if (DDOS_FLAG):
-            ph = p * 10
+            print(f"{YELLOW}DDOS{RESET}")
+            ph = ph * 10
             
-        #PH ante dia festivo
+        #PH ante dia festivo #OK
         
-        a1 = porcentaje_aumento_evento(11, 20)
-        a2 = porcentaje_aumento_evento(6, 1)
-        a3 = porcentaje_aumento_evento(10, 1)
-        a4 = porcentaje_aumento_evento(12, 24)
+        a1 = porcentaje_aumento_evento_anticipado(11, 20)
+        a2 = porcentaje_aumento_evento_anticipado(6, 1)
+        a3 = porcentaje_aumento_evento_anticipado(10, 1)
+        a4 = porcentaje_aumento_evento_anticipado(12, 24)
         
         a5 = porcentaje_aumento_evento(11, 29)
         a6 = porcentaje_aumento_evento(5, 13)
         a7 = porcentaje_aumento_evento(5, 14)
         a8 = porcentaje_aumento_evento(5, 15)
         a9 = porcentaje_aumento_evento(12, 2)
-        
+
         ph = ph*a1*a2*a3*a4*a5*a6*a7*a8*a9
         
-        #PH ante evento trimestral
-        if es_comienzo_trimestre()
+        #PH ante evento trimestral #OK
+        if es_comienzo_trimestre() == True:
+            print(f"{BLUE}COMIENZO MES{RESET}")
             ph = ph * 15
         
 
         ### GENERO, CALCULO O USO TODO LO QUE SALE ###
         
         #mantenimiento activo
-        if MANTENIMIENTO_ACTIVO > 0
-            MANTENIMIENTO_ACTIVO--
-        else
+
+        """
+        if MANTENIMIENTO_ACTIVO >= 1:
+            MANTENIMIENTO_ACTIVO = MANTENIMIENTO_ACTIVO - 1
+        else:
             sin_mantenimiento()
             
         #mantenimiento mensual
         
-        if es_comienzo_mes()
+        if es_comienzo_mes() == True:
             mantenimiento()
             
         #mantenimiento por ataque ddos
         
-        if ph >= 2*CAP_CPU or ph >= 2*CAP_RAM
+        if MANTENIMIENTO_ACTIVO == 0 and (ph >= 2*CAP_CPU or ph >= 2*CAP_RAM):
             mantenimiento()
-
+        """
         ### MODIFICO VAR DE ESTADO CON TODO LO QUE SALE Y ENTRA ###
 
-        P = P + ph - min(CAP_CPU, CAP_RAM)
-        
+        print(f"PH: {ph}")
+       
+        print(f"CPU: {CAP_CPU}")
+        print(f"RAM: {CAP_RAM}")
+
+        P = min(CAP_CPU, CAP_RAM) - ph
+        print(f"P: {P}")
         #hay rechazados
-        if P<0
+
+        if P<0:
+            print(f"{RED}T: {T}{RESET}")
             CVCS = CVCS + 1
             SPPR = SPPR + abs(P)
-            if ph>CAP_CPU
-                SPCF = SPCF + ph - CAP_CPU
-            if ph>CAP_RAM
-                SPRF = SPRF + ph - CAP_RAM
 
+            if ph>CAP_CPU:
+                SPCF += ph - CAP_CPU
+
+            if ph>CAP_RAM:
+                SPRF += ph - CAP_RAM
+        else:
+            print(f"{GREEN}T: {T}{RESET}")
+                
         P = 0
 
         if T < TF:
